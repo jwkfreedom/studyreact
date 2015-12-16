b3d1d86d4e40cf03c0d4dbd6f800f664f4e17a9d
jiangweikun <jwk@aa.com>
2015-12-16 16:48:54 +0800

    add material-ui

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # studyreact
+
+
+## Tips
+- 需要修改react-router的package.json里history版本号
+  "history": "1.13.x"  -> "history": "^1.13.x"
--- src/shared/components/AppHandler.js
+++ src/shared/components/AppHandler.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+const AppBar = require('material-ui/lib/app-bar');
+const IconButton = require('material-ui/lib/icon-button');
+const FlatButton = require('material-ui/lib/flat-button');
+const NavigationClose = require('material-ui/lib/svg-icons/navigation/close');
 export default class AppHandler extends React.Component {  
   render() {
-    return <div>Hello App Handler3</div>;
+    return <div>
+    <AppBar
+  title={<span>Title</span>}
+  iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+  iconElementRight={<FlatButton label="Save" />} />
+    <h1>Hello App Handler4</h1></div>;
   }
 }
